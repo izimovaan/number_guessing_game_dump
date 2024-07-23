47e72f94cf9a5dcf7036ab734a9d343528b4c39d
darwin watterson <izimovaan@gmail.com>
2024-07-23 11:45:59 +0000

    feat: game prompt function

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+PROMPT () {
+  echo "Enter your username:"
+  read USERNAME
+
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+  if [[ -z $USER_ID ]]
+  then
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+    USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+  else
+    GAMES_PLAYED=$($PSQL "SELECT count(user_id) FROM games WHERE user_id=$USER_ID")
+    BEST_GAME=$($PSQL "SELECT min(number_of_guesses) FROM games WHERE user_id=$USER_ID")
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took <best_game> guesses."
+  fi
+}
