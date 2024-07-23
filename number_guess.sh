c7430e347cee8c6a0aee5ce1fa7aa5137ac9d732
darwin watterson <izimovaan@gmail.com>
2024-07-23 12:13:41 +0000

    feat: main game logic function added

--- number_guess.sh
+++ number_guess.sh
@@ -2,6 +2,37 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+GAME () {
+  SECRET=$(( $RANDOM % 1000 + 1 ))
+  echo "Guess the secret number between 1 and 1000:"
+  ATTEMPTS=0
+  GUESSED=0
+
+  while [[ $GUESSED == 0 ]]
+  do
+    read GUESS
+
+    if [[ ! $GUESS =~ ^[0-9]+$ ]]
+    then
+      echo "That is not an integer, guess again:"
+    elif [[ $GUESS == $SECRET ]]
+    then
+      ATTEMPTS=$(( $ATTEMPTS + 1 ))
+      echo "You guessed it in $ATTEMPTS tries. The secret number was $SECRET. Nice job!"
+      INSERT_GAMES_RESULT=$($PSQL "INSERT INTO games(user_id, number_of_guesses) VALUES($USER_ID, $ATTEMPTS)")
+      GUESSED=1
+    elif [[ $GUESS -lt $SECRET ]]
+    then
+      ATTEMPTS=$(( $ATTEMPTS + 1 ))
+      echo "It's higher than that, guess again:"
+    elif [[ $GUESS -gt $SECRET ]]
+    then
+      ATTEMPTS=$(( $ATTEMPTS + 1 ))
+      echo "It's lower than that, guess again:"
+    fi
+  done
+}
+
 PROMPT () {
   echo "Enter your username:"
   read USERNAME
@@ -15,6 +46,10 @@ PROMPT () {
   else
     GAMES_PLAYED=$($PSQL "SELECT count(user_id) FROM games WHERE user_id=$USER_ID")
     BEST_GAME=$($PSQL "SELECT min(number_of_guesses) FROM games WHERE user_id=$USER_ID")
-    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took <best_game> guesses."
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
+
+  GAME
 }
+
+PROMPT
